05306061d923c371e52b7fb07a1479b76d68e39c
Illusion <109838848+Illusionna@users.noreply.github.com>
2023-12-08 23:51:41 +0800

    Delete Transformer directory

--- Transformer/main.py
+++
@@ -1,7 +0,0 @@
-from utils.Seed.Main import MAIN
-
-"""
-请先运行 main.py 查看文档.
-"""
-
-print(MAIN.__doc__)
--- Transformer/utils/ViT/vit_pytorch/__init__.py
+++
@@ -1,5 +0,0 @@
-from utils.ViT.vit_pytorch.vit import ViT
-from utils.ViT.vit_pytorch.simple_vit import SimpleViT
-
-from utils.ViT.vit_pytorch.mae import MAE
-from utils.ViT.vit_pytorch.dino import Dino
